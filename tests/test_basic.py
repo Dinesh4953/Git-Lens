afaa68cc9d6a907bd162770cadecbc04ea8c7a2b
Dinesh4953 <dinu08642@gmail.com>
2025-12-14 15:09:03 +0530

    Update test_basic.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add project root to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 def test_app_import():
     import app
     assert app is not None
